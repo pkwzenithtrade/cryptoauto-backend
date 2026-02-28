5c20abff923f8833e4ce6f1d8a5ba41aaddaa6cb
pkwzenithtrade <pkwzenith@gmail.com>
2026-02-28 16:52:58 -0300

    Refactor server.js to use async MongoDB connection
    Refactor server initialization to use async function for MongoDB connection and server start.

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const authRoutes = require('./src/routes/auth.routes');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -9,16 +8,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB conectado"))
-  .catch(err => console.log(err));
-
-app.get('/', (req, res) => {
-  res.json({ message: "CryptoAuto Backend + MongoDB 🚀" });
-});
+const authRoutes = require('./src/routes/auth.routes');
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.use('/auth', authRoutes);
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB conectado");
+
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Erro ao conectar no MongoDB:", error);
+  }
+}
+
+startServer();
