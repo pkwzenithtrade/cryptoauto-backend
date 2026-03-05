0d68a8139791c365a5557e76e16254f0f4301034
pkwzenithtrade <pkwzenith@gmail.com>
2026-03-05 15:22:34 -0300

    Delete models/Portfolio.js

--- models/Portfolio.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const PortfolioSchema = new mongoose.Schema({
-  userId: String,
-  coin: String,
-  amount: Number
-})
-
-module.exports = mongoose.model("Portfolio", PortfolioSchema)
