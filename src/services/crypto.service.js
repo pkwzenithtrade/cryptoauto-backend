ba903c51747fe51482f0957eddcbebd7912ed624
pkwzenithtrade <pkwzenith@gmail.com>
2026-03-06 13:06:11 -0300

    Update crypto.service.js

--- src/services/crypto.service.js
+++ src/services/crypto.service.js
@@ -1,27 +1,48 @@
 const axios = require("axios")
 
+let cache = {}
+let lastFetch = {}
+
 async function getCryptoPrice(coin) {
+
+ const now = Date.now()
+
+ // cache por 30 segundos
+ if (cache[coin] && (now - lastFetch[coin] < 30000)) {
+  console.log("USANDO CACHE:", coin)
+  return cache[coin]
+ }
 
  try {
 
   const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`
 
-  console.log("URL COINGECKO:", url)
+  console.log("BUSCANDO API:", url)
 
   const response = await axios.get(url)
 
-  console.log("RESPOSTA API:", response.data)
+  cache[coin] = response.data
+  lastFetch[coin] = now
 
   return response.data
 
  } catch (error) {
 
-  console.log("ERRO COMPLETO:", error.message)
+  if (error.response && error.response.status === 429) {
 
-  if (error.response) {
-   console.log("STATUS:", error.response.status)
-   console.log("DATA:", error.response.data)
+   console.log("LIMITE COINGECKO ATINGIDO")
+
+   if (cache[coin]) {
+    return cache[coin]
+   }
+
+   return {
+    [coin]: { usd: 0 }
+   }
+
   }
+
+  console.log("ERRO:", error.message)
 
   throw new Error("Erro ao buscar preço da cripto")
 
