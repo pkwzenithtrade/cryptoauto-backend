c06a8dc19292497b56ee00f5044bd3c749ce2944
pkwzenithtrade <pkwzenith@gmail.com>
2026-03-05 21:39:10 -0300

    Implement error handling in getCryptoPrice
    Added error handling to getCryptoPrice function.

--- src/services/crypto.service.js
+++ src/services/crypto.service.js
@@ -2,11 +2,22 @@ const axios = require("axios")
 
 async function getCryptoPrice(coin) {
 
- const response = await axios.get(
-  `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`
- )
+ try {
 
- return response.data
+  const response = await axios.get(
+   `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`
+  )
+
+  return response.data
+
+ } catch (error) {
+
+  return {
+   error: "Erro ao buscar preço"
+  }
+
+ }
+
 }
 
 module.exports = {
