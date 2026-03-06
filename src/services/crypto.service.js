fec3d6527875c9f8aae4fb2dffc86e86abf333c7
pkwzenithtrade <pkwzenith@gmail.com>
2026-03-06 10:42:33 -0300

    Update crypto.service.js

--- src/services/crypto.service.js
+++ src/services/crypto.service.js
@@ -2,21 +2,11 @@ const axios = require("axios")
 
 async function getCryptoPrice(coin) {
 
- try {
+ const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`
 
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`
+ const response = await axios.get(url)
 
-  const response = await axios.get(url)
-
-  return response.data
-
- } catch (error) {
-
-  console.log("ERRO COINGECKO:", error.message)
-
-  return null
-
- }
+ return response.data
 
 }
 
