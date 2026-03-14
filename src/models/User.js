e078d368eba3e4360bad94528ac68fb2d03da1c2
pkwzenithtrade <pkwzenith@gmail.com>
2026-03-14 00:30:51 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+
   email: {
     type: String,
-    unique: true
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, "Email inválido"]
   },
-  password: String,
+
+  password: {
+    type: String,
+    required: true
+  },
+
   plan: {
     type: String,
+    enum: ["free", "pro", "premium"],
     default: "free"
   }
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
