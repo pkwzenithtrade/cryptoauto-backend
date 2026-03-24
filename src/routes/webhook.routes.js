a10cd1e9871d1d9c17cf1b2a79022efd5a82eaec
pkwzenithtrade <pkwzenith@gmail.com>
2026-03-24 19:32:07 -0300

    Update webhook.routes.js

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.model");
+const User = require("../models/User");
 
 // 🔐 TOKEN DO ASAAS
 const ASAAS_WEBHOOK_TOKEN = process.env.ASAAS_WEBHOOK_TOKEN;
