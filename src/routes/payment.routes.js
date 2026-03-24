2b8c927026863f6446101252619ab137856c74fe
pkwzenithtrade <pkwzenith@gmail.com>
2026-03-24 19:26:53 -0300

    Update payment.routes.js

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-const User = require("../models/user.model");
+const User = require("../models/User");
 
 const {
   createCustomer,
