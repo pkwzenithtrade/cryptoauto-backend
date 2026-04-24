d747ee3d00e489abe5f141c2c3d119d7c6856569
pkwzenithtrade <pkwzenith@gmail.com>
2026-04-24 20:43:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const axios = require("axios");
 
 // 🔥 MODELS
 const User = require("./src/models/User");
-const UserStats = require("./models/UserStats.js");
+const UserStats = require("./models/UserStats");
 
 // 🔥 SERVICES
 const { scanOpportunities } = require("./src/ai/opportunityHunter");
